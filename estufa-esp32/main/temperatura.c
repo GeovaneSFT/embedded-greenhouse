b920435a86230e438c5e8b64565e76943e9d43e1
Geovane Freitas <galefreitas@gmail.com>
2024-08-30 21:55:05 -0300

    Atualiza temperatura.c para modelo esp-idf

--- estufa-esp32/main/temperatura.c
+++ estufa-esp32/main/temperatura.c
@@ -1,30 +1,44 @@
 #include <math.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
 #include "esp_log.h"
 
-int sensorPin = 36;
 
-double Thermister(int RawADC)
-{
-    double Temp;
-    Temp = log(((10240000 / RawADC) - 10000));
-    Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp)) * Temp);
-    Temp = Temp - 273.15;
-    return Temp;
+#define TAG "THERMISTOR"
+
+int sensorPin = 36
+
+
+void init_adc() {
+	adc1_config_width(ADC_WIDTH_BIT_12);
+	adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
 }
 
-void setup()
-{
-    // codigo setup
-    Serial.begin(9600);
+double Thermister(int RawADC){
+	double Temp;
+	Temp = log(((10240000/RawADC) - 10000));
+	Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp)) * Temp);
+	Temp = Temp - 273.15;
+	return Temp;
 }
 
-void loop()
-{
-    // codigo principal que vai repetir infinito
-    int adcValue = analogRead(sensorPin);
+/* void setup() {
+	// codigo setup
+	Serial.begin(9600);
+}
+*/
 
-    Serial.print(Thermister(adcValue));
-    Serial.println(" ºC");
+void app_main() {
+	init_adc();
+	
+	while(1) {
+		int adcValue = adc1_get_raw(ADC1_CHANNEL_0);
 
-    delay(500);
+		double temperature = Thermister(adcValue);
+		ESP_LOGI(TAG, "Temperatura: %.2f ºC", temperature);
+
+		vTaskDelay(pdMS_TO_TICKS(500));
+	}
 }
